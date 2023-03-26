877e6962b3b7007bb95dc044d1c42938e45116a3
Jason Suarez <swarecito@gmail.com>
2023-03-26 19:42:27 +0200

    👌 IMPROVE: change some css to make it nicer on device landscape mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,10 @@ const Header = () => {
         content="Recal is the best, neuroscience based, way to scheudle a study planning and remember things on the long run. We handle the scheduling and the optimization part so can be more efficient and productive."
       />
       <link rel="icon" href="/memory-recall.png" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover"
+      />
     </Head>
   );
 }
